f3152698e7acafce2a08aedfaa4c45b98d37821e
Łukasz Boczkaja <lboczkaja@unity-t.pl>
2022-03-07 17:28:27 +0100

    works but returns hostname only

--- which_pve.py
+++ which_pve.py
@@ -3,15 +3,30 @@
 import pve_credentials
 import proxmoxer
 import argparse
+import sys
 
-pve_con = proxmoxer.ProxmoxAPI(pve_credentials.pve_host, user=pve_credentials.pve_user, password=pve_credentials.pve_password, verify_ssl=False)
+proxmox_api = proxmoxer.ProxmoxAPI(pve_credentials.pve_host, user=pve_credentials.pve_user, backend="ssh_paramiko")
 
 # lists all vms in cluster
-for pve_node in pve_con.nodes.get():
-    for vm in pve_con.nodes(pve_node['node']).qemu.get():
-        print(pve_node['node'])
-        print("{}. {} => {}".format(vm['firewall'], vm['name'], vm['status']))
+
+pve_hosts = [ "192.168.2.125", "192.168.2.126", "192.168.7.21", "192.168.7.22", "192.168.253.184", "192.168.7.17", "10.22.127.11", "10.22.127.12", "10.22.127.13", "192.168.7.24"]
 
 parser = argparse.ArgumentParser(description="Get hypervisor running given VM as argument")
 
 parser.add_argument('vm_name', help="virtual machine name")
+
+args = parser.parse_args()
+vm_found = False
+
+for pve_host in pve_hosts:
+    proxmox_api = proxmoxer.ProxmoxAPI(pve_host, user="root", backend="ssh_paramiko")
+    for pve_node in proxmox_api.nodes.get():
+        for vm in proxmox_api.nodes(pve_node['node']).qemu.get():
+            if vm['name'] == args.vm_name:
+                print("{} -> https://{}:8006".format(pve_node['node'], pve_host))
+                vm_found = True
+                sys.exit(0)
+
+
+if not vm_found:
+    print('No such VM found in pve list')
